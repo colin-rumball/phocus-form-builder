5d87b385c39bbcf530807c25083997bdee209b5f
Colin Rumball <colin.rumball@gmail.com>
2024-02-08 15:32:26 -0500

    updating auth.config for convex on prod

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://discrete-goldfish-67.clerk.accounts.dev",
+      domain: "https://clerk.phocus.studio",
       applicationID: "convex",
     },
   ],
