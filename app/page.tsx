adf060cccd36a1b861e7dfcc0b22e883316709c7
Colin Rumball <colin.rumball@gmail.com>
2024-02-08 21:10:28 -0500

    set homepage image to priority

--- app/page.tsx
+++ app/page.tsx
@@ -37,6 +37,7 @@ export default async function HomePage() {
           quality={100}
           sizes="100vw"
           className="object-cover brightness-[0.8]"
+          priority
         />
       </div>
     </Page>
